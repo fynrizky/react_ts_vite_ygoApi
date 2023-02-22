83c552f7f5372d65e4859de5fcc4e78cc16ad5b3
fynrizky <fiyaniky25@gmail.com>
2023-02-22 16:32:54 +0700

    menambahkan fungsi search berdasarkan desc sebelumnya hanya name

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,8 @@ const App: React.FC = () => {
 
 
   const filteredData = cards.filter((item) => {
-    return item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase());
+    return item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+           item.desc.toLowerCase().includes(searchTerm.toLocaleLowerCase());
   })
 
 
